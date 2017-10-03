72d03c9c4c80e5e736b4ce438b27297fa87cc05d
bsroddy <bsroddy@knox.edu>
2017-10-03 23:45:04 +0100

    Calculator screen, buttons, and layout adjustments
    Added calculator screen; made it so now there are the following buttons:
    numbers 0-9, (+/-), decimal point, clear, four arithmetic operations,
    and the equals button; and made the layout readable and usable on
    desktop.

--- calculator.py
+++ calculator.py
@@ -5,17 +5,29 @@ from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
+from kivy.config import Config
 
-class Numpad(Widget):
-    """The numpad, digits 0-9. No other buttons."""
+Config.set('graphics', 'width', '110')
+Config.set('graphics', 'height', '150')
+
+
+class ButtonPad(Widget):
+    """The buttons of the calculator. Contains digits 0-9, the four arithmetic operations,
+     the clear button, and the equals button."""
     pass
 
+class Screen(Widget):
+    """The area of the calculator that will display the numbers the user enters and processes."""
+    pass
+
+class Calculator(Widget):
+    """The widget that combines the Screen and ButtonPad."""
+    pass
 
 class CalculatorApp(App):
     """The main app."""
     def build(self):
-        """Currently only displays the numpad."""
-        return Numpad()
+        return Calculator()
 
 if __name__ == '__main__':
     CalculatorApp().run()
