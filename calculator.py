ec360a125a2d1e080f0e5c46056bc23b311192e2
bsroddy <bsroddy@knox.edu>
2017-10-07 21:32:45 +0100

    Condensed classes to one: Calculator
    To fit with the .kv file, Screen and ButtonPad classes were deleted.
    The methods allocated to Screen (get_screen_text(), set_screen_text(),
    clear_screen_text(), add_digit_to_screen(), and enter_button()) were
    reimplemented in the Calculator class.  This follows what I understand
    are the intended conventions of the kv language.
    
    Interaction with Calculator now more robust: layout now spans the entire
    window, pressing number buttons enters digits on the screen, and the
    clear button works.

--- calculator.py
+++ calculator.py
@@ -7,22 +7,17 @@ from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.config import Config
 
+class Calculator(BoxLayout):
+    """The widget that combines the Screen and ButtonPad."""
 
-class ButtonPad(Widget):
-    """The buttons of the calculator. Contains digits 0-9, the four arithmetic operations,
-     the clear button, and the equals button."""
-    pass
-
-class Screen(Widget):
-    """The area of the calculator that will display the numbers the user enters and processes."""
     def get_screen_text(self):
-        return self.ids['screen_text'].text
+        return self.ids["screen"].text
 
     def set_screen_text(self, new_text):
-        self.ids['screen_text'].text = new_text
+        self.ids['screen'].text = new_text
 
     def clear_screen_text(self):
-        self.ids['screen_text'].text = ''
+        self.ids['screen'].text = ''
 
     def add_digit_to_screen(self, digit):
         current_screen_text = self.get_screen_text()
@@ -33,9 +28,9 @@ class Screen(Widget):
             if button_entry.isdigit():
                 self.add_digit_to_screen(button_entry)
 
-class Calculator(Widget):
-    """The widget that combines the Screen and ButtonPad."""
-    pass
+            if button_entry == 'C':
+                self.clear_screen_text()
+
 
 class CalculatorApp(App):
     """The main app."""
